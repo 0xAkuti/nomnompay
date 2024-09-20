f32339c6606030e856cbbebccac97b70b2ce77dd
Akuti <40723201+0xAkuti@users.noreply.github.com>
2024-09-20 23:10:42 +0800

    impl: use wallet set id from constant

--- circle_api.py
+++ circle_api.py
@@ -8,10 +8,11 @@ dotenv.load_dotenv()
 
 CIRCLE_API_KEY = os.getenv("CIRCLE_API_KEY")
 ENTITY_SECRET = os.getenv("ENTITY_SECRET")
+WALLET_SET_ID = os.getenv("WALLET_SET_ID")
 
 client = circle_utils.init_developer_controlled_wallets_client(api_key=CIRCLE_API_KEY, entity_secret=ENTITY_SECRET)
 
-def create_wallet(nr_wallets: int, wallet_set_id: str, blockchain: str = 'MATIC-AMOY'):
+def create_wallet(nr_wallets: int, blockchain: str = 'MATIC-AMOY') -> list[developer_controlled_wallets.SCAWallet]:
     api_instance = developer_controlled_wallets.WalletsApi(client)
 
     if nr_wallets > 20:
@@ -22,7 +23,7 @@ def create_wallet(nr_wallets: int, wallet_set_id: str, blockchain: str = 'MATIC-
             "accountType": 'SCA',
             "blockchains": [blockchain],
             "count": nr_wallets,
-            "walletSetId": wallet_set_id
+            "walletSetId": WALLET_SET_ID
         })
         api_instance.create_wallet(request) # TODO does not actually return wallets as it's supposed to
 
