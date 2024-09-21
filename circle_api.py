fee9c15ce386ed76bf47dcf71ab1153e82428139
Akuti <40723201+0xAkuti@users.noreply.github.com>
2024-09-22 01:53:16 +0800

    refac: get rid of circle sdk

--- circle_api.py
+++ circle_api.py
@@ -2,57 +2,80 @@ import uuid
 import enum
 import dotenv
 import os
-
-from circle.web3 import developer_controlled_wallets
-from circle.web3 import utils as circle_utils
 import requests
 
 import definitions as defs
 from constants import *
 
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+import base64
+import web3
+from eth_abi import decode
+
 dotenv.load_dotenv()
 
 CIRCLE_API_KEY = os.getenv("CIRCLE_API_KEY")
-ENTITY_SECRET = os.getenv("ENTITY_SECRET")
+ENTITY_SECRET = os.getenv("ENTITY_SECRET", "")
 WALLET_SET_ID = os.getenv("WALLET_SET_ID")
+with open("data/setup/key.pub", "r") as f:
+    PUBLIC_KEY = f.read()
 
-client = circle_utils.init_developer_controlled_wallets_client(api_key=CIRCLE_API_KEY, entity_secret=ENTITY_SECRET)
+def generate_entity_secret_ciphertext():
+    entity_secret = bytes.fromhex(ENTITY_SECRET)
+    if len(entity_secret) != 32:
+        raise Exception("invalid entity secret")
+
+    # encrypt data by the public key
+    public_key = RSA.importKey(PUBLIC_KEY)
+    cipher_rsa = PKCS1_OAEP.new(key=public_key, hashAlgo=SHA256)
+    encrypted_data = cipher_rsa.encrypt(entity_secret)
+
+    # encode to base64
+    ciphertext = base64.b64encode(encrypted_data)
+
+    return ciphertext.decode()
 
 def create_wallet(nr_wallets: int, blockchain: defs.Blockchain = defs.Blockchain.MATIC_AMOY) -> defs.Wallets:
-    api_instance = developer_controlled_wallets.WalletsApi(client)
-
-    if nr_wallets > 20:
-        raise ValueError("Cannot create more than 20 wallets at a time")
-
-    try:
-        request = developer_controlled_wallets.CreateWalletRequest.from_dict({
-            "accountType": 'SCA',
-            "blockchains": [blockchain.value],
-            "count": nr_wallets,
-            "walletSetId": WALLET_SET_ID
-        })
-        api_instance.create_wallet(request) # TODO does not actually return wallets as it's supposed to
-
-        return [wallet.actual_instance for wallet in api_instance.get_wallets(page_size=nr_wallets).data.wallets] # type: ignore
-
-    except developer_controlled_wallets.ApiException as e:
-        print(f"Exception when calling WalletsApi->create_wallet: {e}")
-        return []
-
-def update_wallet(wallet_id: str, wallet_name: str, wallet_ref_id: str) -> bool:
-    api_instance = developer_controlled_wallets.WalletsApi(client)
+    if nr_wallets > 200:
+        raise ValueError("Cannot create more than 200 wallets at a time")
     
-    try:
-        request = developer_controlled_wallets.UpdateWalletRequest(
-                name=wallet_name,
-                ref_id=wallet_ref_id
-            )
-        api_instance.update_wallet(wallet_id, request)
-        print(f"Updated wallet {wallet_id} with name: {wallet_name} and ref_id: {wallet_ref_id}")
-        return True
-    except developer_controlled_wallets.ApiException as e:
-        print(f"Exception when calling WalletsApi->update_wallet: {e}")
-        return False
+    url = "https://api.circle.com/v1/w3s/developer/wallets"
+
+
+    payload = {
+        "idempotencyKey": str(uuid.uuid4()),
+        "accountType": "SCA",
+        "blockchains": [blockchain.value],
+        "count": nr_wallets,
+        "entitySecretCiphertext": generate_entity_secret_ciphertext(),
+        "walletSetId": WALLET_SET_ID
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": f"Bearer {CIRCLE_API_KEY}"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    return defs.Wallets.parse_obj(response.json()['data'])
+
+def update_wallet(wallet_id: str, wallet_name: str, wallet_ref_id: str):
+    url = f"https://api.circle.com/v1/w3s/wallets/{wallet_id}"
+
+    payload = {
+        "name": wallet_name,
+        "refId": wallet_ref_id
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": f"Bearer {CIRCLE_API_KEY}"
+    }
+
+    response = requests.put(url, json=payload, headers=headers)
+    return response.json()
 
 def get_wallet_balance(wallet_id: str):
     url = f"https://api.circle.com/v1/w3s/wallets/{wallet_id}/balances"
@@ -79,7 +102,7 @@ def send_transfer(wallet_id: str, recipient: str, tokenId: str, amount: float, r
         "tokenId": tokenId,
         "amounts": [str(amount)],
         "idempotencyKey": str(uuid.uuid4()), # TODO create a uuid from the user request so that it can only be sent once
-        "entitySecretCiphertext": client.generate_entity_secret_ciphertext(),
+        "entitySecretCiphertext": generate_entity_secret_ciphertext(),
         "feeLevel": "MEDIUM",
         "refId": ref_id
     }
@@ -111,7 +134,7 @@ def execute_smart_contract(wallet_id: str, contract_address: str, abi_function_s
         "abiFunctionSignature": abi_function_signature,
         "abiParameters": abi_parameters,
         "idempotencyKey": str(uuid.uuid4()),
-        "entitySecretCiphertext": client.generate_entity_secret_ciphertext(),
+        "entitySecretCiphertext": generate_entity_secret_ciphertext(),
         "feeLevel": "MEDIUM"
     }
 
